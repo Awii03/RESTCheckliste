8ac0c69c50d900b0ad9a6e0df4589aa9eeb16df8
Amr78 <Amr78@AWAIS>
2024-04-09 12:59:58 +0200

    Add project files.

---
+++ RESTCheckliste/Class1.cs
@@ -0,0 +1,108 @@
+﻿namespace RESTCheckliste
+{
+    public interface IPublisher
+    {
+       public string PublisherName { get; set; }
+
+    }
+
+
+    public class Movie
+    {
+        //Properties
+        public string Name { get; set; }
+        public int Year { get; set; }
+        public IPublisher Publisher { get; set; }
+
+        //Constructor
+        public Movie(string name, int year, IPublisher publisher)
+        {
+            Name= name;
+            Year= year;
+            Publisher = publisher;
+
+        }
+
+        //Methode til at vise (display) informaation omkring movie
+        public void DisplayMovieInfo()
+        {
+            Console.WriteLine($"Movie:{Name}");
+            Console.WriteLine($"Year:{Year}");
+            Console.WriteLine($"Publisher:{Publisher}");
+
+        }
+
+    }
+    public class Publisher : IPublisher
+    {
+        public string Name { get; set; }
+       
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
---
+++ RESTCheckliste/Program.cs
@@ -0,0 +1,56 @@
+﻿using RESTCheckliste;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Program
+{
+    static void Main()
+    {
+        // Creating instances of the Movie class
+        var publisher1 = new Publisher { PublisherName  = "Warner Bros." };
+        var publisher2 = new Publisher { PublisherName  = "Castle Rock Entertainment" };
+
+        Movie movie1 = new Movie("Inception", 2010, publisher1);
+        Movie movie2 = new Movie("The Shawshank Redemption", 1994, publisher2);
+
+        // Displaying movie information
+        Console.WriteLine("Movie 1:");
+        movie1.DisplayMovieInfo();
+
+        Console.WriteLine("\nMovie 2:");
+        movie2.DisplayMovieInfo();
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
